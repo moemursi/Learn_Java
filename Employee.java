9c9fa02e3bc10ddd4a8caf253f7c0e0717e7fef8
Mohamed ELmursi <elmu0006@algonquinlive.com>
2021-11-05 20:39:33 -0400

    inital commit
    Employee Class + UML

---
+++ Employee.java
@@ -0,0 +1,29 @@
+public class Employee {
+   private int id;
+   private double salary;
+
+   //get id of employee
+    public int getId() {
+        return id;
+    }
+
+    //set id of employee
+    public void setId(int idNum) {
+        this.id = idNum;
+
+    }
+
+    //get salary of employee
+    public double getSalary() {
+        return salary;
+    }
+
+    //set salary of employee
+    public void setSalary(double sal) {
+        this.salary = sal;
+    }
+
+
+
+
+}
