e0b9e446ef4de060814def2ba5796742dfff003a
Mohamed ELmursi <elmu0006@algonquinlive.com>
2021-11-05 21:14:17 -0400

    Create EmployeeWithTerritory.java
    Added Subclass "Employee With Territory"

---
+++ EmployeeWithTerritory.java
@@ -0,0 +1,14 @@
+public class EmployeeWithTerritory extends Employee {
+    private int territory;
+
+    //get territory
+    public int getTerritory() {
+        return territory;
+    }
+    
+    //set territory
+    public void setTerritory(int terr) {
+        this.territory = terr;
+    }
+
+}
